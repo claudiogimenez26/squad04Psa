074df1eca02dbd44dc6d6132784786d6eda934a7
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2024-11-25 15:55:33 -0300

    merge implementacion consultas del backend con el front

---
+++ frontend/app/consultar-cargas/page.tsx
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+"use client";
+
+import { Datepicker } from "flowbite-react";
+import { useState } from "react";
+
+export default function () {
+  const [fechaInicio, setFechaInicio] = useState<Date>(new Date());
+  const [fechaFin, setFechaFin] = useState<Date>(new Date());
+
+  return (
+    <div className="flex justify-center items-center gap-4">
+      <span>Desde el</span>
+      <Datepicker
+        className="w-80"
+        language="es-AR"
+        labelTodayButton="Hoy"
+        labelClearButton="Limpiar"
+        maxDate={new Date()}
+      />
+      <span>hasta el</span>
+      <Datepicker
+        className="w-80"
+        language="es-AR"
+        labelTodayButton="Hoy"
+        labelClearButton="Limpiar"
+        maxDate={new Date()}
+      />
+    </div>
+  );
+=======
+export default function () {
+  return <div>Testing</div>;
+>>>>>>> 0aca13b (wip: modularización inicial del formulario de carga de horas)
+}
