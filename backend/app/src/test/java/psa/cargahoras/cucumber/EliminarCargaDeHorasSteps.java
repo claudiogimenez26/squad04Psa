f87d9022b0d839ba640432b7c95f1675fc457805
claudiogimenez26 <cgimenez@fi.uba.ar>
2024-11-26 18:50:58 -0300

    se agrega step para eliminar cargad e horas

---
+++ backend/app/src/test/java/psa/cargahoras/cucumber/EliminarCargaDeHorasSteps.java
@@ -0,0 +1,25 @@
+package psa.cargahoras.cucumber;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+import io.cucumber.java.es.Y;
+import java.util.Arrays;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import psa.cargahoras.dto.RecursoDTO;
+import psa.cargahoras.dto.TareaDTO;
+import psa.cargahoras.entity.CargaDeHoras;
+import psa.cargahoras.repository.CargaDeHorasRepository;
+import psa.cargahoras.service.ApiExternaService;
+import psa.cargahoras.service.CargaDeHorasService;
+
+
+public class EliminarCargaDeHorasSteps {
+    
+}
